49607989836b60074e0e5fc27c2444ec8584d6c9
nsanju0413 <nsanju013@gmail.com>
2024-10-21 18:18:45 -0400

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb+srv://root:Buggargs$9100@cluster0.kqixp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
